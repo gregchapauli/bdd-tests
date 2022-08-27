e11be447113c4c93386ec9ccb430cb7b75cb554b
Gregory Caloin <gregchapauli@gmail.com>
2022-08-27 11:43:07 +0200

    Initialisation du test

--- features/support/step.js
+++ features/support/step.js
@@ -2,6 +2,12 @@
 const { Given, When, Then } = require("@cucumber/cucumber");
 const assert = require("assert").strict;
 
-Given("a variable set to {int}", function (number) {});
-When("I increment the variable by {int}", function (number) {});
-Then("the variable should be {int}", function (number) {});
+Given("a variable set to {int}", function (number) {
+  this.setTo(number);
+});
+When("I increment the variable by {int}", function (number) {
+  this.incrementBy(number);
+});
+Then("the variable should be {int}", function (number) {
+  assert.equal(this.variable, number);
+});
